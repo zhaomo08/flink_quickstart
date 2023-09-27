fbfc0a10e5d98d7991f843bb7ba05ca177584a6a
zhaojiacheng <zhaomo0823@gmail.com>
2023-09-27 13:34:42 +0800

    test

--- src/main/java/saymeevetime/DataStreamJob.java
+++ src/main/java/saymeevetime/DataStreamJob.java
@@ -40,6 +40,7 @@ import org.apache.flink.util.Collector;
  */
 public class DataStreamJob {
 
+
 	public static void main(String[] args) throws Exception {
 		// Sets up the execution environment, which is the main entry point
 		// to building Flink applications.
